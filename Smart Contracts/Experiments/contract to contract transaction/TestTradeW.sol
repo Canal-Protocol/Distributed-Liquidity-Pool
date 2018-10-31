e62e8280aee7033903132f8f3f9245a60dc79015
Anmol Singh <ads365@users.noreply.github.com>
2018-10-31 17:50:24 +1100

    comments

--- Smart Contracts/Experiments/contract to contract transaction/TestTradeW.sol
+++ Smart Contracts/Experiments/contract to contract transaction/TestTradeW.sol
@@ -1,9 +1,8 @@
 pragma solidity 0.4.18;
 
-// EXPERIMENTAL ONLY - Funds will be FROZEN
+// EXPERIMENTAL ONLY
 
-//functions to allow for kyber reserve token wallet to be a smart contract.
-
+// test for integration with Kyber Reserve - replicating the send dest tokens component. 
 
 interface ERC20 {
     function totalSupply() public view returns (uint supply);
@@ -24,16 +23,20 @@ interface TradeWalletInterface {
 contract TestTradeW {
 
     TradeWalletInterface public TradeWallet;
-
+    
+    /// @dev Constructor sets the TradeWallet address
+    /// @ param _TradeWallet is the funding source wallet (TradeWallet will be integrate with FundWallet in the future).
     function TestTradeW (TradeWalletInterface _TradeWallet) public {
         TradeWallet = _TradeWallet;
     }
 
+    /// @dev replicates the send dest tokens where the token address is an ERC20
     function tokenPuller (ERC20 destToken, address destAddress, uint destAmount) public {
         TradeWallet.pullToken(destToken, destAmount, destAddress);
 
     }
 
+    /// @dev replicates the send dest tokens where the token address is Ether
     function etherPuller (address destAddress, uint destAmount) public {
         TradeWallet.pullEther(destAmount, destAddress);
     }
