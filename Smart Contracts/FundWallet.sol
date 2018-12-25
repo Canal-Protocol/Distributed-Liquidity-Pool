543279d1cc343db1ef260f9642bb7f7eddee4001
Anmol Singh <anmoldeepsingh365@gmail.com>
2018-12-25 14:43:19 +1100

    truffle

--- Smart Contracts/FundWallet.sol
+++ Smart Contracts/FundWallet.sol
@@ -4,16 +4,7 @@ pragma solidity 0.4.18;
 /// @dev Not fully tested, use only in test environment.
 
 
-interface ERC20 {
-    function totalSupply() public view returns (uint supply);
-    function balanceOf(address _owner) public view returns (uint balance);
-    function transfer(address _to, uint _value) public returns (bool success);
-    function transferFrom(address _from, address _to, uint _value) public returns (bool success);
-    function approve(address _spender, uint _value) public returns (bool success);
-    function allowance(address _owner, address _spender) public view returns (uint remaining);
-    function decimals() public view returns(uint digits);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
-}
+import "./ERC20Interface.sol";
 
 contract FundWallet {
 
